aafceb4734b89abb3a41242dd094aa33d3e4acba
Dhruv Shah <dhruvshah5784@gmail.com>
2020-05-06 22:46:49 -0400

    svg loader

--- js/main.js
+++ js/main.js
@@ -96,3 +96,22 @@ jQuery(function ($) {
     // sr.reveal(".featured-projects", { viewFactor: 0.1 });
     // sr.reveal(".other-projects", { viewFactor: 0.05 });
 });
+
+
+
+const preloader = document.querySelector('.preloader');
+const fadeEffect = setInterval(() => {
+  // if we don't set opacity 1 in CSS, then
+  // it will be equaled to "" -- that's why
+  // we check it, and if so, set opacity to 1
+  if (!preloader.style.opacity) {
+    preloader.style.opacity = 1;
+  }
+  if (preloader.style.opacity > 0) {
+    preloader.style.opacity -= 0.1;
+  } else {
+    clearInterval(fadeEffect);
+  }
+}, 200);
+
+window.addEventListener('load', fadeEffect);
